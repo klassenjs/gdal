73b30c2253bc65213a13f6ba107c67f873c2a5d0
Jim Klassen <klassen.js@gmail.com>
2019-09-05 12:12:59 -0500

    Increase max open datasets in a VRT

--- gcore/gdalproxypool.cpp
+++ gcore/gdalproxypool.cpp
@@ -512,7 +512,7 @@ void GDALDatasetPool::Ref()
     {
         int l_maxSize =
             atoi(CPLGetConfigOption("GDAL_MAX_DATASET_POOL_SIZE", "100"));
-        if (l_maxSize < 2 || l_maxSize > 1000)
+        if (l_maxSize < 2 || l_maxSize > 4000)
             l_maxSize = 100;
 
         // Try to not consume more than 25% of the usable RAM
