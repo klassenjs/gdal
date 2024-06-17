98a0b5275cafd99202f6284516e5a8551db46d50
Jim Klassen <klassen.js@gmail.com>
2024-06-17 13:26:20 -0500

    gdalinfo_bin: Set exit code if there were errors while processing.

--- apps/gdalinfo_bin.cpp
+++ apps/gdalinfo_bin.cpp
@@ -74,6 +74,8 @@ static void Usage()
 MAIN_START(argc, argv)
 
 {
+    int nRetCode = 0;
+
     EarlySetConfigOptions(argc, argv);
 
     /* -------------------------------------------------------------------- */
@@ -234,6 +236,9 @@ MAIN_START(argc, argv)
 
         CPLFree(pszGDALInfoOutput);
 
+	if (CPLGetErrorCounter() > 0)
+		nRetCode = 1;
+
         GDALClose(hDataset);
 #ifdef __AFL_HAVE_MANUAL_CONTROL
     }
@@ -247,7 +252,7 @@ MAIN_START(argc, argv)
 
     GDALDestroy();
 
-    exit(0);
+    exit(nRetCode);
 }
 
 MAIN_END
